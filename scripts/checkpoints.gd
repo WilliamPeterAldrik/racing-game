70a7901b8f60f55c500a577e940255004b94a64d
KhaylaAP <khayla.allegra@gmail.com>
2026-06-15 07:34:36 +0700

    fixed time in HUD

--- scripts/checkpoints.gd
+++ scripts/checkpoints.gd
@@ -9,11 +9,13 @@ var max_laps = 3
 var next_checkpoint_index = 0
 var lap_times = []
 var start_time = 0.0
+var lap_start_time = 0.0
 
 @onready var checkpoints = self # Path ke folder penyimpan Area3D
 
 func _ready():
 	start_time = Time.get_ticks_msec() / 1000.0
+	lap_start_time = start_time
 	
 	# Menghubungkan semua checkpoint secara otomatis
 	var index = 0
@@ -36,9 +38,9 @@ func _on_checkpoint_entered(body: Node3D, cp_index: int):
 
 func _lap_selesai():
 	# Hitung waktu putaran
-	var current_time = (Time.get_ticks_msec() / 1000.0) - start_time
-	lap_times.append(current_time)
-	print("Lap ", current_lap, " Selesai! Waktu: ", current_time, " detik")
+	var now = Time.get_ticks_msec() / 1000.0
+	var lap_time = now - lap_start_time
+	lap_times.append(lap_time)
 	
 	if current_lap >= max_laps:
 		print("Balapan Selesai! Kamu Hebat!")
@@ -46,11 +48,10 @@ func _lap_selesai():
 		race_finished.emit(lap_times)
 		# Nanti di sini kamu bisa panggil UI buat nunjukin skor akhir
 	else:
-		lap_completed.emit(current_lap, current_time)
+		lap_completed.emit(current_lap, lap_time)
 		current_lap += 1
 		next_checkpoint_index = 0
-		start_time = Time.get_ticks_msec() / 1000.0
-		 # Reset waktu buat lap selanjutnya
+		lap_start_time = now  # 
 
 func get_elapsed_time() -> float:
 	return (Time.get_ticks_msec() / 1000.0) - start_time
